2a741bf73cc2e1f5c382529ec970c1b9bf653427
sujanst98644 <sujan.st98644@gmail.com>
2023-05-28 15:26:44 +0545

    QA-2 testing

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -4,7 +4,7 @@ import React, { useState } from 'react'
 const LandingPage = () => {
   return (
     <div className='w-full h-screen bg-gradient-to-b from-[#C77DFF] to-[#3C096C] '>
-      <h1>LandingPage</h1>
+      <h1>LandingPage 2</h1>
     </div>
   )
 }
