3d45f763a61a53d4fc263f1c69ffe8d41bebb62a
ishabaral <baralisha.16@gmail.com>
2023-05-28 13:44:43 +0545

    QA-1 basic change

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react'
 
 const LandingPage = () => {
   return (
-    <div className='w-full h-screen'>
+    <div className='w-full h-screen bg-gradient-to-b from-[#C77DFF] to-[#3C096C] '>
       <h1>LandingPage</h1>
     </div>
   )
