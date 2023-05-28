eed4aff0512c9e01a770b4d61b3a120db75db6e2
ishabaral <baralisha.16@gmail.com>
2023-05-28 11:44:43 +0545

    added Landing and footer pages

---
+++ src/components/LandingPage.tsx
@@ -0,0 +1,12 @@
+'use client'
+import React, { useState } from 'react'
+
+const LandingPage = () => {
+  return (
+    <div className='w-full h-screen bg-gradient-to-b from-[#C77DFF] to-[#3C096C] '>
+      <h1>LandingPage</h1>
+    </div>
+  )
+}
+
+export default LandingPage
