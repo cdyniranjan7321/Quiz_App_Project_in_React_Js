6f48453852736b26dd97141985fce8a1b6490023
ishabaral <baralisha.16@gmail.com>
2023-05-31 17:00:03 +0545

    QA-2 added settings page

---
+++ src/app/settings/page.tsx
@@ -0,0 +1,15 @@
+import Navbar from '@/components/Navbar'
+import Sidebar from '@/components/Sidebar'
+import React from 'react'
+
+const Settings = () => {
+  return (
+    <div>
+      <Navbar />
+      <Sidebar />
+      <h1>Settings</h1>
+    </div>
+  )
+}
+
+export default Settings
