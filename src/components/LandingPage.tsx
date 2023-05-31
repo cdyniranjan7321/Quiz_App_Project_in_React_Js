b391527bdb578636e7d32dda9da38b86d5b703ec
SundarShrestha273 <sundarshrestha273@gmail.com>
2023-05-31 16:45:07 +0545

    QA-4 created sidebar and navbar

---
+++ src/components/LandingPage.tsx
@@ -0,0 +1,18 @@
+"use client"
+import React, { useState } from "react"
+import Navbar from "./Navbar"
+import Sidebar from "./Sidebar"
+import Footer from "./Footer"
+
+const LandingPage = () => {
+  return (
+    <div className=' bg-gradient-to-b from-[#C77DFF] to-[#3C096C] h-[100%] w-full absolute '>
+     <Navbar />
+     <Sidebar />
+     {/* <Footer /> */}
+       
+    </div>
+  )
+}
+
+export default LandingPage
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='bg-gradient-to-b from-[#C77DFF] to-[#3C096C]  text-center  w-full h-[12%] '>
+        <h1 className='text-4xl font-bold pt-2 pb-1 text-white'> Quiz App</h1>
+      </div>
+  )
+}
+
+export default Navbar
