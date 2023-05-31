871eaec9299a2b8fcd3fbf3620b70f398676500d
ishabaral <baralisha.16@gmail.com>
2023-05-31 16:57:46 +0545

    QA-2 merge pull request

--- src/components/LandingPage.tsx
+++ src/components/LandingPage.tsx
@@ -1,16 +1,15 @@
-"use client"
-import React, { useState } from "react"
-import Navbar from "./Navbar"
-import Sidebar from "./Sidebar"
-import Footer from "./Footer"
+'use client'
+import React, { useState } from 'react'
+import Navbar from './Navbar'
+import Sidebar from './Sidebar'
+import Footer from './Footer'
 
 const LandingPage = () => {
   return (
     <div className=' bg-gradient-to-b from-[#C77DFF] to-[#3C096C] h-[100%] w-full absolute '>
-     <Navbar />
-     <Sidebar />
-     {/* <Footer /> */}
-       
+      <Navbar />
+      <Sidebar />
+      {/* <Footer /> */}
     </div>
   )
 }
