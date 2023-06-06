d9393040031ee08e518819df46de41644587f179
SundarShrestha273 <sundarshrestha273@gmail.com>
2023-06-06 15:36:47 +0545

    QA-6 Created round selection page

--- src/app/round/page.tsx
+++ src/app/round/page.tsx
@@ -1,39 +1,37 @@
-import Navbar from '@/components/Navbar'
-import Sidebar from '@/components/Sidebar'
-import Image from 'next/image'
-import React from 'react'
+"use client";
+import Navbar from "@/components/Navbar"
+import Sidebar from "@/components/Sidebar"
+import Image from "next/image"
+import React from "react"
 
 const Round = () => {
   return (
-    <div className=' h-screen w-full'>
+    <div className='h-screen w-full'>
       <Navbar title='Select Round' />
       <Sidebar />
-      <div className='bg-gradient-to-b from-[#EED8FF] to-[#3E0C6E]  top-0 relative left-[5%] w-[95%] h-[88%]'>
-        <div className='flex flex-col items-center justify-center'>
-          <h1 className='text-black-500 text-3xl'> Rounds Selection</h1>
-          <div className='flex '>
-            <div className='flex flex-col'>
-              <div className='bg-white rounded-md mt-6 pl-2 text-bold  pt-1 text-2xl h-9 w-[80%]'>
-                {' '}
-                <button>General Round</button>
-              </div>
-              <div className='bg-white rounded-md pl-3  mt-6 text-2xl  pt- h-15 w-[60%] '>
-                <button> Rapid Fire</button>
-              </div>
-              <div className='bg-white rounded-md   pl-2 mt-6  text-2xl h-15  w-[90%]'>
-                <button> Multiple Question</button>
-              </div>
-              <div className='bg-white rounded-md  pl-2 mt-5 text-2xl h-15  w-30  pr-2'>
-                {' '}
-                <button>General Knowledge</button>
-              </div>
-              <div className='pl-12 pt-12  '>
-                <button className='text-2xl font-bold text-blue-500 h-10 w-20  bg-yellow-500 '>
-                  {' '}
-                  Next
-                </button>
-              </div>
+      <div className='bg-gradient-to-b from-[#EED8FF] to-[#3E0C6E] top-0  left-[5%] w-[100%] h-[88%]'>
+        <div className="flex flex-row justify-around align-items-center pt-[13%] md:pl-[12%] md:pt-[19%] lg:pt-[7%]">
+          <div className="flex-col justify-content:center align-items:center pt-11">
+            <div className='bg-white rounded-md mt-6 pl-2 text-bold text-2xl h-9 w-[80%]'>
+              {" "}
+              <button>General Round</button>
             </div>
+            <div className='bg-white rounded-md pl-2 mt-6 text-2xl pt- h-15 w-[60%]'>
+              <button>Rapid Fire</button>
+            </div>
+            <div className='bg-white rounded-md pl-1 mt-7 pt-1 text-2xl h-10 w-[90%]'>
+              <button>Multiple Question</button>
+            </div>
+            <div className='bg-white rounded-md pl-2 mt-5 text-2xl h-9 w-[100%] pr-2'>
+              <button>General Knowledge</button>
+            </div>
+            <div className=' pt-12'>
+              <button className='text-2xl font-bold text-black h-10 w-[26%] bg-[#57CC99] rounded-md'>
+                Next
+              </button>
+            </div>
+          </div>
+          <div className=" hidden md:block">
             <Image
               height='400'
               width='400'
