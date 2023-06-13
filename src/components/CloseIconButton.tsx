db5470da3d63c02e50ed31ffe229b2dd42141e44
sujanst98644 <ramkumarthapa2022@gmail.com>
2023-06-13 11:02:00 +0545

    modifying icon

--- src/components/CloseIconButton.tsx
+++ src/components/CloseIconButton.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { MdClose as CloseIcon } from "react-icons/md";
 const CloseIconButton: React.FC = () => {
   return (
-    <div className="font-white-300 text-gray-200 text-bold text-4xl">
+    <div className="text-red-600 text-8xl">
       <button>
         <CloseIcon />
       </button>
