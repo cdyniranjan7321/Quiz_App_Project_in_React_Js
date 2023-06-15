449206dba59472892a8a6c3e08fb583ac2037ede
sujanst98644 <ramkumarthapa2022@gmail.com>
2023-06-15 23:26:24 +0545

    added generalquestionpasspage

--- src/app/generalQuestionPass/page.tsx
+++ src/app/generalQuestionPass/page.tsx
@@ -3,11 +3,11 @@ import React from 'react'
 import Navbar from '@/components/Navbar'
 import Question from '@/components/Question'
 import { useSearchParams } from 'next/navigation'
-const GeneralQuestionsA = () => {
+const GeneralQuestionsPass = () => {
   const searchParams = useSearchParams()
   const questionNumber = searchParams.get('questionNumber')
-  const questionNum = questionNumber !== null ? parseInt(questionNumber) : 0;
-//to conditionally check if the number is null since it came from somewhere
+  const questionNum = questionNumber !== null ? parseInt(questionNumber) : 0
+  //to conditionally check if the number is null since it came from somewhere
   return (
     // starting whole page
     <div className='h-screen w-screen overflow-hidden flex bg-blue-gray-900 bg-gradient-to-b from-gray-100 to-purple-900'>
@@ -16,10 +16,10 @@ const GeneralQuestionsA = () => {
         {/* start navbar  and main page*/}
         <Navbar title='General round' isGeneralQuestionsPage={true} />
         {/* end navbar */}
-        <Question isGeneralAPage={true} questionNum={questionNum} />
+        <Question isGeneralQuestionsPassPage={true} questionNum={questionNum} />
       </div>
       {/* ending navbar and main page  */}
     </div>
   )
 }
-export default GeneralQuestionsA
+export default GeneralQuestionsPass
