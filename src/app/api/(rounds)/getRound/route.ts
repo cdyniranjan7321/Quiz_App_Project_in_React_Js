8e4fd3c26cec0ea89f0d353207dfba12c0e57a9b
ishabaral <baralisha.16@gmail.com>
2023-06-19 15:58:24 +0545

    QA-17 cleared console

--- src/app/api/(rounds)/getRound/route.ts
+++ src/app/api/(rounds)/getRound/route.ts
@@ -4,7 +4,6 @@ import prisma from "../../../../../prisma/client"
 export async function GET(){
     try{
         const rounds = await prisma.round.findMany()
-        console.log("rounds :", rounds)
         return NextResponse.json(rounds)
     } catch(err) {
         return NextResponse.json(err)
