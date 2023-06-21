5facde5250e1906f9a684412262b096ee095f732
ishabaral <baralisha.16@gmail.com>
2023-06-21 14:40:22 +0545

    QA-23 code refining

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -6,7 +6,7 @@ const Loading = () => {
   let [loading, setLoading] = useState(true)
   let [color, setColor] = useState('#57CC99')
   return (
-    <div className='w-full h-screen items-center justify-center bg-gradient-to-b from-[#C77DFF] to-[#3C096C] '>
+    <div className='w-full h-screen flex items-center justify-center bg-gradient-to-b from-[#C77DFF] to-[#3C096C] '>
       <ClipLoader
         color={color}
         loading={loading}
