a8005c71d48580309b0dadf59e62dd71a69bbed9
ishabaral <baralisha.16@gmail.com>
2023-06-23 12:04:13 +0545

    QA-23 removed unnecessary code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,6 @@
 import LandingPage from '@/components/LandingPage'
-import axios from 'axios'
-
-async function getPosts() {
-  // const res = await fetch(`${process.env.BASE_URL}/api/getPosts`)
-
-  // if (!res.ok) {
-  //   console.log(res)
-  // }
-  // return res.json()
-
-  await axios
-    .get(`${process.env.BASE_URL}/api/getPosts`)
-    .then((response) => {
-      console.log(response)
-    })
-    .catch((err) => {
-      console.log(err)
-    })
-}
 
 export default async function Home() {
-  const data = await getPosts()
-  console.log('data : ', data)
   return (
     <main className=' '>
       <LandingPage />
