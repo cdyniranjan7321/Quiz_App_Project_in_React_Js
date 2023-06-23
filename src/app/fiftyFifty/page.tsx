7030c3f41c94dc5a074127e36a6eef84cc74d60f
ishabaral <baralisha.16@gmail.com>
2023-06-23 12:09:33 +0545

    QA-23 renamed files without -

---
+++ src/app/fiftyFifty/page.tsx
@@ -0,0 +1,34 @@
+'use client'
+import React from 'react'
+import Navbar from '@/components/Navbar'
+import { useSearchParams } from 'next/navigation'
+import AvailableQuestions from '@/components/AvailableQuestions'
+
+const FiftyFifty = () => {
+  const searchParams = useSearchParams()
+  const totalQuestions = searchParams.get('totalquestions')
+  const roundId = searchParams.get('roundId')
+  const round_id = roundId !== null ? parseInt(roundId) : 0
+  const total_questions = totalQuestions !== null ? parseInt(totalQuestions) : 0
+
+  return (
+    // starting whole page
+    <div className='h-screen w-screen overflow-hidden flex bg-blue-gray-900 bg-gradient-to-b from-gray-100 to-purple-900'>
+      {/* starting navbar and main page  */}
+      <div className='flex flex-col w-full'>
+        {/* start navbar  and main page*/}
+        <Navbar title='Fifty-fifty Round' isBackArrow={true} />
+        {/* end navbar */}
+
+        <AvailableQuestions
+          isMultipleQuestionsPage={true}
+          totalQuestions={total_questions}
+          roundId={round_id}
+          roundName='fiftyFifty'
+        />
+      </div>
+      {/* ending navbar and main page  */}
+    </div>
+  )
+}
+export default FiftyFifty
