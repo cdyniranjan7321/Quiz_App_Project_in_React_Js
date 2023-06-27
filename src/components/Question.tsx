0a88b1ced30e86fb7798d28b9b9eaeccd1bdd9f2
sujanst98644 <ramkumarthapa2022@gmail.com>
2023-06-27 13:53:02 +0545

    modified question page

--- src/components/Question.tsx
+++ src/components/Question.tsx
@@ -12,16 +12,16 @@ const Question = (props: AvailableProps) => {
   const { isGeneralAPage, isRapidFirePage, qn } = props
   const [showText, setShowText] = useState(false)
   const [showAnswer, setShowAnswer] = useState(false)
-  const [passCount,setPassCount]=useState(0)
+  const [passCount, setPassCount] = useState(0)
   const handlePassButtonClick = () => {
     setShowText(true)
-    setPassCount((prevCount)=>prevCount+1)
+    setPassCount((prevCount) => prevCount + 1)
   }
-  let timerStartFrom = 30;
+  let timerStartFrom = 30
   if (passCount === 1) {
-    timerStartFrom = 20;
+    timerStartFrom = 20
   } else if (passCount > 1) {
-    timerStartFrom = 15;
+    timerStartFrom = 15
   }
   return (
     <div className='flex flex-col justify-center'>
@@ -71,37 +71,38 @@ const Question = (props: AvailableProps) => {
           </div>
         )}
       </div>
-
-      <div className='flex justify-center  mb-2 rounded-2xl mr-10 px-7 py-4 text-xl'>
-        <Timer startFrom={timerStartFrom}/>
-      </div>
-      <div className=' flex justify-center mt-2 mb-4'>
-        <button
-          className=' bg-blue-400 rounded-2xl mr-10 px-7 py-4 w-auto text-xl'
-          onClick={() => setShowAnswer(!showAnswer)}
-        >
-          {showAnswer ? <span>Hide Answer</span> : <span>Show Answer</span>}
-        </button>
-        <button className=' bg-green-500 rounded-2xl mr-10 px-7 py-4 w-32 text-xl'>
-          Correct
-        </button>
-        <button className='bg-red-500 rounded-2xl mr-10 px-7 py-4 w-32 text-white text-xl'>
-          Incorrect
-        </button>
-        <button
-          className='bg-custom-brown rounded-2xl mr-10 px-7 py-4 w-32 text-white text-xl'
-          onClick={handlePassButtonClick}
-        >
-          Pass
-        </button>
-        {isRapidFirePage && (
+      <div className='fixed bottom-6 left-0 right-0 '>
+        <div className='flex justify-center  mb-2 rounded-2xl mr-10 px-7 py-4 text-xl'>
+          <Timer startFrom={timerStartFrom} />
+        </div>
+        <div className=' flex justify-center'>
           <button
-            className=' bg-blue-700 rounded-2xl mr-10 px-7 py-4 w-32 text-white text-xl'
+            className=' bg-blue-400 rounded-2xl mr-10 px-7 py-4 w-auto text-xl'
+            onClick={() => setShowAnswer(!showAnswer)}
+          >
+            {showAnswer ? <span>Hide Answer</span> : <span>Show Answer</span>}
+          </button>
+          <button className=' bg-green-500 rounded-2xl mr-10 px-7 py-4 w-32 text-xl'>
+            Correct
+          </button>
+          <button className='bg-red-500 rounded-2xl mr-10 px-7 py-4 w-32 text-white text-xl'>
+            Incorrect
+          </button>
+          <button
+            className='bg-custom-brown rounded-2xl mr-10 px-7 py-4 w-32 text-white text-xl'
             onClick={handlePassButtonClick}
           >
-            Next
+            Pass
           </button>
-        )}
+          {isRapidFirePage && (
+            <button
+              className=' bg-blue-700 rounded-2xl mr-10 px-7 py-4 w-32 text-white text-xl'
+              onClick={handlePassButtonClick}
+            >
+              Next
+            </button>
+          )}
+        </div>
       </div>
     </div>
   )
