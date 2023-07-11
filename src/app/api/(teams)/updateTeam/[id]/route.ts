530bf48e0148ce425a955be9a35218f78f9aa5a6
ishabaral <baralisha.16@gmail.com>
2023-07-11 11:58:47 +0545

    QA-28 added update api for teams

---
+++ src/app/api/(teams)/updateTeam/[id]/route.ts
@@ -0,0 +1,25 @@
+import { NextResponse } from 'next/server'
+import prisma from '../../../../../../prisma/client'
+
+interface Team {
+  teamName?: string
+  gameOrder?: number
+}
+
+export async function PATCH(request: Request, context: { params: any }) {
+  try {
+    const team_id = context.params.id
+    const id = team_id !== null ? parseInt(team_id) : 0
+    const res: Team = await request.json()
+
+    const data = await prisma.teams.update({
+      where: { id },
+      data: res,
+    })
+    return NextResponse.json('Updated Successfully !')
+  } catch (error) {
+    if (error.name === 'PrismaClientKnownRequestError') {
+      return NextResponse.json('Sorry, provided teamId does not exist !')
+    } else return NextResponse.json(error)
+  }
+}
