b32ad75eb2a0dd6e46c240903462e43442333503
sujanst98644 <ramkumarthapa2022@gmail.com>
2023-07-28 15:48:18 +0545

    modified number page

--- src/components/AvailableQuestions.tsx
+++ src/components/AvailableQuestions.tsx
@@ -11,6 +11,39 @@ type AvailableProps = {
 const AvailableQuestions = (props: AvailableProps) => {
   const { isMultipleQuestionsPage, totalQuestions, roundId, roundName } = props
   console.log(' roundName : ', roundName)
+  const [teamData, setteamData] = useState<Array<{ id: number; teamName: string;}>>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState('');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('/api/getTeams'); // Make a GET request to the server-side API route
+        const data = await response.json();
+        if (response.ok) {
+          setteamData(data.teams);
+        } else {
+          setError(data.error || 'Failed to fetch data');
+        }
+        setIsLoading(false);
+      } catch (error) {
+        setError('Error fetching data. Please try again later.');
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
+  }, []);
+  const teamName1 = teamData.length > 0 ? teamData[0].teamName : '';
+  const teamName2 = teamData.length > 0 ? teamData[1].teamName : '';
+  // const teamName3 = teamData.length > 0 ? teamData[2].teamName : '';
+  // const teamName4 = teamData.length > 0 ? teamData[3].teamName : '';
+  // const teamName5 = teamData.length > 0 ? teamData[4].teamName : '';
+  // const teamName6 = teamData.length > 0 ? teamData[5].teamName : '';
+  // const teamName7 = teamData.length > 0 ? teamData[6].teamName : '';
+  // const teamName8 = teamData.length > 0 ? teamData[7].teamName : '';
+  // const teamName9 = teamData.length > 0 ? teamData[8].teamName : '';
+  // const teamName10 = teamData.length > 0 ? teamData[9].teamName : '';
   const [numRows, setNumRows] = useState(5)
   //this number determines how many rows are shown
   const [numCols, setNumCols] = useState(10)
@@ -58,7 +91,7 @@ const AvailableQuestions = (props: AvailableProps) => {
             )} */}
             <div className='flex'>
               <span className='bg-gray-900 bg-gradient-to-b from-gray-700 to-purple-900 text-white p-2 rounded-lg text-xl my-4 ml-5'>
-                Round for: Red house{' '}
+                Round for: {teamName1}
                 <button className='ml-4 mr-2 mb-1 bg-red-500 w-12 h-6 rounded-xl py-2'></button>
               </span>
             </div>
@@ -80,7 +113,7 @@ const AvailableQuestions = (props: AvailableProps) => {
         <div className='flex flex-col items-center bg-gray-900 bg-gradient-to-b from-gray-700 to-purple-900 text-white mt-4 mr-8 rounded-lg pl-3 pr-2 py-4 ml-auto'>
           <span className='font-italiana text-xl'>Next question for:</span>
           <span>
-            Blue house{' '}
+            {teamName2}
             <button className='ml-2 bg-blue-500 w-12 h-6 rounded-xl py-2'></button>
           </span>
         </div>
