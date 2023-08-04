13cf1a6ee186dbb7f36a128774106999034a1afb
sujanst98644 <ramkumarthapa2022@gmail.com>
2023-08-04 11:42:22 +0545

    QA-39 added folder adminhome

---
+++ src/app/(admin)/adminhome/page.tsx
@@ -0,0 +1,14 @@
+'use client'
+import React, { useState } from 'react'
+import Image from 'next/image'
+import AdminSidebar from '@/components/AdminSidebar'
+const AdminHome = () => {
+  return (
+    <div >
+      <div>
+      <AdminSidebar/>      
+      </div>
+     </div>
+  )
+}
+export default AdminHome
